fdc9a11862952b4804af9c7196850099cde2a8a3
botball <botball@deadrobots.com>
2017-02-27 21:00:09 -0500

    Adjusted to use ET with hay stack rotation.

--- actions.py
+++ actions.py
@@ -2,14 +2,20 @@ import motorsPlusPlus as x
 import utils as u
 import constants as c
 from wallaby import enable_servos, msleep, seconds, analog, motor
+from wallaby import *
 
 headToHead = True
 
 def test():
     print('testing')
-    u.move_servo_timed(c.servoArm, c.armUp, 10)
-    x.drive_speed(70,50)
-    msleep(300)
+    enable_servos()
+    # u.move_servo_timed(c.servoArm, c.armUp, 10)
+    # u.waitForButton()
+    x.drive_speed(85,100)
+    # motor_power(c.LMOTOR, 100)
+    # motor_power(c.RMOTOR, 100)
+    # msleep(11000)
+    u.DEBUG()
     # x.rotate(90, 25)
     # from wallaby import *
     # motor(c.LMOTOR,50)
@@ -64,6 +70,7 @@ def init():
     u.waitForButton()
     c.startTime = seconds()
 
+
 def getFirstHay():
     x.drive_speed(1.5, 50)
     u.move_servo(c.servoClaw, c.clawClose, 50)
@@ -82,12 +89,14 @@ def seeWall():
     return analog(0) < 1000
 
 def seeHay():
-    return analog(0) < 1550
+    return analog(0) < 2200
 
 def turnToHay():
     x.drive_speed(5, -50)
     x.rotate(90, 50)
     x.drive_condition(100,100,seeHay)
+    center()
+    x.drive_speed(-2, 50)
     msleep(500)
 
 def stackHay():
@@ -114,19 +123,48 @@ def stackHay():
     # u.move_servo(c.servoClaw, c.clawOpen, 50)
     # msleep(300)
 
+def seeObject():
+    print analog(0)
+    return analog(0) > 2000
+
+def seeObjectAndWait():
+    return not seeObject() and u.getWait()
+
+def center():
+    x.drive_condition(25, -25, seeObject)
+    u.setWait(1.5)
+    x.drive_condition(-25, 25, seeObjectAndWait)
+    x.pivot_left(5, -50)
+
 def turnToSecondHay():
     x.pivot_right(-110,50)
+    x.drive_speed(.5, -50)
     x.pivot_left(-112,50)
+    x.drive_condition(100, 100, seeHay)
+    center()
+    x.drive_speed(-2, 50)
+    msleep(500)
 
 def stackSecondHay():
-    x.drive_speed(6.2, 50)
-    msleep(500)
+    # x.drive_speed(6.2, 50)
+    # msleep(500)
     u.move_servo(c.servoArm, c.armCube, 35)
     msleep(300)
     u.move_servo(c.servoClaw, c.clawMid, 50)
     msleep(300)
     u.move_servo(c.servoClaw, c.clawOpen, 50)
     msleep(300)
+
+    x.drive_speed(1, -50)
+    # x.rotate(5, 50)
+    msleep(300)
+    u.move_servo(c.servoArm, c.armDown, 40)
+    msleep(300)
+    x.drive_speed(2.11, 40)
+    u.move_servo(c.servoClaw, c.clawClose, 50)
+    msleep(300)
+    x.drive_speed(3, -50)
+    u.move_servo(c.servoArm, c.armUp, 50)
 
 
 
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import utils as u
 def main():
     print "running"
     act.init()
-    #act.test()
+    # act.test()
     act.getFirstHay()
     act.goToFarWall()
     act.turnToHay()
